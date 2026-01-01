582abc4bc9c8b3551bccfbd852a6d5e4267fadf4
KS <kp1718@duck.com>
2026-01-01 09:16:03 -0600

    Add favorites and search to forecasts with UX polish and wording updates

---
+++ iOSWeather/Keyboard.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+func dismissKeyboard() {
+    UIApplication.shared.sendAction(
+        #selector(UIResponder.resignFirstResponder),
+        to: nil,
+        from: nil,
+        for: nil
+    )
+}
